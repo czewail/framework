ce58641f28f5e2512c0e2dd0bfaa90084c53e8a2
czewail <chanzewail@gmail.com>
2019-08-29 09:44:31 +0800

    update: validator test

--- __test__/src/decorators/validator.spec.js
+++ __test__/src/decorators/validator.spec.js
@@ -1,8 +1,8 @@
 require('../../../src/helpers');
-const Validator = require('../../../src/decorators/validator.stage-2');
+const Validator = require('../../../src/decorators/validator');
 
 describe('Validator Decorator', () => {
-  it('should patch type and validatorName in Validator', () => {
+  it('should patch type and name in Validator', () => {
     const Klass = @Validator('example') class Example { };
     expect(Reflect.getMetadata('type', Klass.prototype)).toBe('validator');
     expect(Reflect.getMetadata('name', Klass.prototype)).toBe('example');
